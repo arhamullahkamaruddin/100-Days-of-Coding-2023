774e95b79f780bccde8a9901d4481a14c894487d
Arhamullah Kamaruddin <123912972+arhamullahkamaruddin@users.noreply.github.com>
2023-10-20 19:55:06 +0800

    Hari12.java
    Menampilkan angka ganjil menggunakan perulangan for

---
+++ Hari12.java
@@ -0,0 +1,10 @@
+public class AngkaGanjil {
+    
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 10; i+=3){
+            System.out.println(i);
+        }
+    }
+    
+}
