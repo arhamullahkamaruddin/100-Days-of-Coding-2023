27e8821e80a6e11f5d4d4cfc420f3956bfff322a
Arhamullah Kamaruddin <123912972+arhamullahkamaruddin@users.noreply.github.com>
2023-10-25 22:20:48 +0800

    Hari17.java
    Menampilkan data bigDecimal

---
+++ Hari17.java
@@ -0,0 +1,9 @@
+public class BigDesimal {
+    public static void main(String[] args) {
+        BigDecimal bdDariString = new BigDecimal("0.1");
+        BigDecimal bdDariInt = new BigDecimal(42);
+
+        System.out.println(bdDariString);
+        System.out.println(bdDariInt);
+    }
+}
