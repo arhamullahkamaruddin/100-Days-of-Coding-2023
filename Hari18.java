87adbccb9c5edf76d4f18f354b04cc7acc684867
Arhamullah Kamaruddin <123912972+arhamullahkamaruddin@users.noreply.github.com>
2023-10-26 22:24:07 +0800

    Hari18.java
    Mengupdate value sebuah variabel

---
+++ Hari18.java
@@ -0,0 +1,10 @@
+public class UpdateVariable {
+    
+    public static void main(String[] args) {
+        int angka = 1;
+        angka = 9;
+        
+        System.out.println(angka);
+    }
+    
+}
