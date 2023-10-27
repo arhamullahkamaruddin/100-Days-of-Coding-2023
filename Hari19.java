540ca2a9d337b70e53ce55eb8b6ba2b8784ecbe0
Arhamullah Kamaruddin <123912972+arhamullahkamaruddin@users.noreply.github.com>
2023-10-28 01:16:30 +0800

    Hari19.java
    Belajar java serta menampilkan nilainya berdasarkan indeks

---
+++ Hari19.java
@@ -0,0 +1,10 @@
+public class BelajarArraJava {
+    
+    public static void main(String[] args) {
+        String nama[] = {"Arham", "Hasna"};
+        System.out.println(nama[0]);
+        System.out.println(nama[1]);
+        
+    }
+    
+}
