aa8a5b393d23dbc409ad237a86ee33610fa3ec67
Arhamullah Kamaruddin <123912972+arhamullahkamaruddin@users.noreply.github.com>
2023-10-28 23:32:40 +0800

    Hari20.java
    belajar array dan menampilkan nilai atau valuenya dengan menggunakan perulangan for

---
+++ Hari20.java
@@ -0,0 +1,9 @@
+public class BelajarArrayJava {
+    public static void main(String[] args) {
+        String nama[] = {"Arham","Hasna"};
+        
+        for (int i = 0; i < nama.length; i++) {
+            System.out.println(nama[i]);
+        }
+    }
+}
