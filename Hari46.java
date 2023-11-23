3e16bc56e6587a0952cd14237a395b3e1926b751
Arhamullah Kamaruddin <123912972+arhamullahkamaruddin@users.noreply.github.com>
2023-11-23 21:14:54 +0800

    Indeks Terakhir
    Menampilkan isi dan indeks terakhir dari suatu array yang tidak diketahui berapa jumlah data di dalam array tersebut.

---
+++ Hari46.java
@@ -0,0 +1,11 @@
+public class IndeksTerakhir {
+    public static void main(String[] args) {
+        int angka[] = {1, 2, 3, 4, 5};
+
+        int IndeksTerakhir = angka.length - 1;
+
+        System.out.println("Indeks Terakhir: " + IndeksTerakhir);
+
+        System.out.println("Nilai pada Indeks Terakhir: " + angka[IndeksTerakhir]);
+    }
+}
