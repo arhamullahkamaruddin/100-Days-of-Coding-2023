c3615f9ea377bde24062560cfdef66d4f0952f9a
Arhamullah Kamaruddin <123912972+arhamullahkamaruddin@users.noreply.github.com>
2023-11-30 23:48:44 +0800

    Mencari Nilai Y
    Membuat program sederhana menyelesaikan soal matematika menggunaan program sederhana

---
+++ Hari53.java
@@ -0,0 +1,10 @@
+public class NilaiY {
+    public static void main(String[] args) {
+        System.out.println("Tentukan nilai y = 5x + 5 jika diketahui nilai x = 12");
+        int x = 12;
+
+        int y = 5 * x + 5;
+
+        System.out.println("Nilai dari y jika x = "+x+" adalah "+y);
+    }
+}
